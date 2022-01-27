59a1738c5371c90a715a5abb9fac0af2747d9d95
RayLin24 <51829855+RayLin24@users.noreply.github.com>
2022-01-27 11:42:26 +0800

    Upload by PicGo

---
+++ imgs202201271142671.sql
@@ -0,0 +1,1 @@
+INSERT INTO `<table_name>` (`id`, `authentication`, `description`, `method`, `name`, `service_name`, `tag`, `url`, `anonymous`, `service_id`) VALUES ('92372131745995e144f920941b224a38', 1, NULL, 'POST', '根据稿件id筛选出可用的稿件', 'media', '原稿-Query', '/news/getEnableSourceIds', NULL, NULL);
